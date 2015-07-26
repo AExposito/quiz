04b8d79ae6c5b74272834da1db78e0a336350ddb
AExposito <posit@MacBook-Pro-de-Alejandro.local>
2015-07-26 23:20:34 +0200

    Lista de Preguntas

--- routes/index.js
+++ routes/index.js
@@ -13,9 +13,16 @@ router.get('/author', function(req, res) {
   res.render('author', { nombre: 'Alejandro Expósito'});
 });
 
+// Definicion de rutas de /quizes
 
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+
+/*
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+*/
 
 
 module.exports = router;
