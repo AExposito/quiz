d12fe92ebd3b257936e5a7125d16843b4919b8ca
AExposito <posit@MacBook-Pro-de-Alejandro.local>
2015-08-05 23:17:01 +0200

    Moderacion de comentarios

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ router.get('/author', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load); //autoload :quizId
-
+router.param('commentId', commentController.load); //autoload :commentId
 // Definición de rutas de sesion
 router.get('/login', sessionController.new);      // formulario login
 router.post('/login', sessionController.create);  // crear sesion
@@ -34,6 +34,8 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizCont
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
+
 /*
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
